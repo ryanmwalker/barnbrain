f8aede02b4d384b545e9b879371599e0c7931334
Ryan M. Walker <41598417+ryanmwalker@users.noreply.github.com>
2018-12-05 19:03:12 -0600

    adding bootstrap themes

---
+++ mod/socia_bootstrap_theme/assets/js/socia_bootstrap_theme.js
@@ -0,0 +1,19 @@
+/**
+ * Twitter&reg; Bootstrap 4.1 Theme for Elgg
+ *
+ * Converts all Elgg css elements to Twitter&reg; Bootstrap elements.  Helps 
+ * Designers create beautiful Bootstrap themes for Elgg.
+ *
+ * @category   Elgg Themes
+ * @author     Shane Barron <admin@socia.us>
+ * @copyright  2018 SocialApparatus / ElggDesign
+ * @version    1.0
+ * @link       http://socia.us
+ */
+
+var show = $.fn.show;
+$.fn.show = function () {
+    this.removeClass('hidden');
+    this.removeClass('hide');
+    return show.call(this);
+};
